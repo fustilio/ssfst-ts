b43956da66937a6f28c38ebc2b78d982ff80c1eb
Francis <francis@u.nus.edu>
2025-02-27 21:56:29 +0800

    Add Vitest configuration and update tests; modify package.json and .gitignore

--- spec/state.spec.js
+++ spec/state.spec.js
@@ -1,42 +1,35 @@
-'use strict';
-
-const State = require('./../src/state');
+const State = require('../src/state');
 
 describe('SSFST State Tests', () => {
-
     it('Invoking the constructor should create an instance', () => {
-        expect(new State()).toEqual(jasmine.any(State));
+        expect(new State()).toBeInstanceOf(State);
     });
 
     it('Should have "isFinal" property set to false after invoking the constructor, without providing arguments.', () => {
-        expect(new State().isFinal).toEqual(false);
+        expect(new State().isFinal).toBe(false);
     });
 
     it('Should have "isFinal" property set to true after invoking the constructor, by providing it with an argument of "true".', () => {
-        expect(new State(true).isFinal).toEqual(true);
+        expect(new State(true).isFinal).toBe(true);
     });
 
     it('Should have "output" property set to "abc" after invoking the constructor, by providing it with an arguments of "true" and "abc".', () => {
-        expect(new State(true, 'abc').output).toEqual('abc');
+        expect(new State(true, 'abc').output).toBe('abc');
     });
 
     it('Should return an instance with a defined "transitions" property with size of 0 after Invoking the constructor, without providing arguments.', () => {
-        expect(new State().transitions.size).toEqual(0);
+        expect(new State().transitions.size).toBe(0);
     });
 
     it('Should return an instance with a defined "output" property with value of "" after invoking the constructor, without providing arguments', () => {
-        expect(new State().output).toEqual('');
+        expect(new State().output).toBe('');
     });
 
     it('Should add a transition when "setTransition" is invoked with valid arguments.', () => {
-        const state = new State();
-        const nextState = new State(true);
-        nextState.output = 'xyz';
-        
-        state.setTransition(nextState, 'a', 'b');
-        const transition = state.processTransition('a');
-        
-        expect(transition.output).toEqual('b');
-        expect(transition.next).toBe(nextState);
+        const state1 = new State();
+        const state2 = new State();
+        state1.setTransition(state2, 'a', '1');
+        expect(state1.transitions.get('a').next).toBe(state2);
+        expect(state1.transitions.get('a').output).toBe('1');
     });
 });
